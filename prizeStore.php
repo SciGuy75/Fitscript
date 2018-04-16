a6bf7e32a53ec7d1297b92f7105ad94c8127c822
Unknown <thejordanmoye@gmail.com>
2018-04-16 18:52:53 -0500

    uploaded initial form for PrizeStore

---
+++ prizeStore.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Prize Store</title></head>
+<?php require_once('navbar.php'); ?>
+<body>
+<div style="padding-left:16px">
+  <h2 align = center>Script Store</h2>
+  <p align = center>Earned a prize? Redeem it here!!</p>
+</div>
+<table border = 5 height = 900 width = 900 align = center>
+<tr>
+<td><img src = "C:\Users\Nikita Ghogare\Pictures\contact.jpg" height = 250 width = 250 border = 5 ><br><input type="button" value="Click Me"></td>
+<td><img src = "C:\Users\Nikita Ghogare\Pictures\contact.jpg" height = 250 width = 250 border = 5 ><br><input type="button" value="Click Me"></td>
+</tr>
+<tr>
+<td><img src = "C:\Users\Nikita Ghogare\Pictures\contact.jpg" height = 250 width = 250 border = 5 ><br><input type="button" value="Click Me"></td>
+<td><img src = "C:\Users\Nikita Ghogare\Pictures\contact.jpg" height = 250 width = 250 border = 5 ><br><input type="button" value="Click Me"></td>
+</tr>
+</table>
+</body>
+</html>
+
+
