2058ffc26260c4d5a681c7cca6e73a57e5e688fb
Ryan O'Malley <reomalley16@yahoo.com>
2018-04-16 20:13:55 -0500

    Challenge class to challenge.php

--- Challenge.php
+++ Challenge.php
@@ -1,5 +1,23 @@
 <?php
-
+class Challenge
+{
+	private $ChallengeID;
+	private $ChallengeType;
+	private $status;
+	private $eligible_for_pts;
+	private $points_reward;
+	
+	function accept(int UserID, int ChallengeID)
+	{
+		
+		return;
+	}
+	
+	function decline(int UserID, string UserName, int $ChallengeID)
+	{
+		return;
+	}
+}
 
 
 ?>
