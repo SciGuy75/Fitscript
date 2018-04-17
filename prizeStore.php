f5738120ee0b40c90440ad366741990efa42ee64
Unknown <thejordanmoye@gmail.com>
2018-04-16 19:46:56 -0500

    PrizeStore flair

--- prizeStore.php
+++ prizeStore.php
@@ -9,19 +9,19 @@ require_once 'navbar.php';
 require_once 'stylesheets.php';
 require_once 'session_check.php'; ?>
 
-<body>
+<body class = "w3-theme">
 <div style="padding-left:16px">
   <h2 align = center>Script Store</h2>
   <p align = center>Earned a prize? Redeem it here!!</p>
 </div>
-<table border = 5 height = 900 width = 900 align = center>
+<table class= "w3-theme-l5" border = 5 height = 900 width = 900 align = 'center'>
 <tr>
-<td><img src = "C:\Users\Nikita Ghogare\Pictures\contact.jpg" height = 250 width = 250 border = 5 ><br><input type="button" value="Click Me"></td>
-<td><img src = "C:\Users\Nikita Ghogare\Pictures\contact.jpg" height = 250 width = 250 border = 5 ><br><input type="button" value="Click Me"></td>
+<td><i class="fa fa-certificate" style="font-size:200px;color:yellow" align = 'center'></i><br><input type="button" value="Click Me"></td>
+<td><i class="fa fa-certificate" style="font-size:200px;color:red"></i><br><input type="button" value="Click Me"></td>
 </tr>
 <tr>
-<td><img src = "C:\Users\Nikita Ghogare\Pictures\contact.jpg" height = 250 width = 250 border = 5 ><br><input type="button" value="Click Me"></td>
-<td><img src = "C:\Users\Nikita Ghogare\Pictures\contact.jpg" height = 250 width = 250 border = 5 ><br><input type="button" value="Click Me"></td>
+<td><i class="fa fa-certificate" style="font-size:200px;color:blue"></i><br><input type="button" value="Click Me"></td>
+<td><i class="fa fa-certificate" style="font-size:200px;color:green"></i><br><input type="button" value="Click Me"></td>
 </tr>
 </table>
 </body>
