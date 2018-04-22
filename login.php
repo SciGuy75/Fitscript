6049c245478d28f527b9e4fcfdb8422782ec6bf3
Unknown <codster303@gmail.com>
2018-04-22 04:10:57 -0500

    login.php works for both local and server

--- login.php
+++ login.php
@@ -1,6 +1,17 @@
 <?php
-  $hn = 'localhost'; 
-  $db = 'dcsp01'; // your NetID
-  $un = 'dcsp01';  // your NetID
-  $pw = 'ab1234!'; // your MySQL password on pluto  
+if (dirname(__FILE__) == 'C:\aaa_master_sync\DCSP\LAB\Fitscritpt'  ){
+    $hn = 'localhost';
+    $db = 'dcsp01'; // your NetID
+    $un = 'root';  // your NetID
+    $pw = ''; // your MySQL password on pluto
+
+}
+else{
+    $hn = 'localhost';
+    $db = 'dcsp01'; // your NetID
+    $un = 'dcsp01';  // your NetID
+    $pw = 'ab1234!'; // your MySQL password on pluto
+    echo dirname(__FILE__);
+
+}
 ?>
