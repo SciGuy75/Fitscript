21963b7268900292789a2d28be5e4f17cf7017fb
Unknown <codster303@gmail.com>
2018-04-22 14:43:07 -0500

    made challenge class into php not C

--- Challenge.php
+++ Challenge.php
@@ -7,13 +7,13 @@ class Challenge
 	private $eligible_for_pts;
 	private $points_reward;
 	
-	function accept(int UserID, int ChallengeID)
+	function accept( $UserID, $ChallengeID)
 	{
 		
 		return;
 	}
 	
-	function decline(int UserID, string UserName, int $ChallengeID)
+	function decline( $UserID,  $UserName, $ChallengeID)
 	{
 		return;
 	}
